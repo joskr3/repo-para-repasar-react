62ab492978d25188131d9b54eeef6c4deb3b78a0
Josue Patricio <josretamozovar@gmail.com>
2025-01-24 21:17:34 -0500

    ultimos cambios

--- src/components/todos-los-regalos.tsx
+++ src/components/todos-los-regalos.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-
 interface ITodosLosRegalosProps {
   totalRegalos: number;
 }
 
 const TodosLosRegalos = ({ totalRegalos }: ITodosLosRegalosProps) => {
-   const [theme, setTheme] = useState("dark");
+  
   return (
     <>
       <p>{totalRegalos}</p>
